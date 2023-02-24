bb1219e7119dc074e5813a96db49cfa335884637
Ryan Haskell-Glatz <ryan.nhg@gmail.com>
2023-02-24 12:18:00 -0600

    check exit code before prompting

--- src/features/elm-format-on-save.ts
+++ src/features/elm-format-on-save.ts
@@ -30,24 +30,23 @@ const provideDocumentFormattingEdits = async (
     return []
   }
 }
-const isWindows = process.platform === "win32"
 
 function runElmFormat(document: vscode.TextDocument): Promise<string> {
-  const command = isWindows
-    ? `npx elm-format --stdin --yes`
-    : `elm-format --stdin --yes`
-
+  const command = `elm-format --stdin --yes`
   const original = document.getText()
   return new Promise((resolve, reject) => {
     const process_ = child_process.exec(command, async (err, stdout, stderr) => {
       if (err) {
-        let response = await vscode.window.showErrorMessage(
-          'Format on save requires "elm-format"',
-          { modal: true, detail: 'Please click "Install" or disable "Format on save" in your settings.' },
-          'Install'
-        )
-        if (response === 'Install') {
-          vscode.commands.executeCommand('elmLand.installElmFormat')
+        const ELM_FORMAT_BINARY_NOT_FOUND = 127
+        if (err.code === ELM_FORMAT_BINARY_NOT_FOUND) {
+          let response = await vscode.window.showErrorMessage(
+            'Format on save requires "elm-format"',
+            { modal: true, detail: 'Please click "Install" or disable "Format on save" in your settings.' },
+            'Install'
+          )
+          if (response === 'Install') {
+            vscode.commands.executeCommand('elmLand.installElmFormat')
+          }
         }
         reject(err)
       } else {
