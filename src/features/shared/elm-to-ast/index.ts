52aeacccb77dd355b3e7a64ba932a86b1549915d
Simon Lydell <simon.lydell@gmail.com>
2023-07-22 11:43:13 +0200

    Queue

--- src/features/shared/elm-to-ast/index.ts
+++ src/features/shared/elm-to-ast/index.ts
@@ -6,69 +6,97 @@ import * as ElmSyntax from './elm-syntax'
 type WorkerState =
   | { tag: 'NotRunning' }
   | { tag: 'Idle', worker: WorkerThreads.Worker }
-  | { tag: 'Busy', worker: WorkerThreads.Worker }
+  | { tag: 'Busy', worker: WorkerThreads.Worker, resolve: (result: ElmSyntax.Ast | undefined) => void, queue: QueueItem[] }
+
+type QueueItem = {
+  rawElmSource: string
+  resolve: (result: ElmSyntax.Ast | undefined) => void,
+}
 
 let workerState: WorkerState = { tag: 'NotRunning' }
 
 export const run = async (rawElmSource: string, token: vscode.CancellationToken): Promise<ElmSyntax.Ast | undefined> => {
   return new Promise((resolve) => {
-    const worker = getWorker(resolve)
-    const newWorkerState: WorkerState = { tag: 'Busy', worker }
     token.onCancellationRequested(() => {
-      if (workerState === newWorkerState) {
-        // Maybe it’s better to never terminate? Just let the thread chug along?
-        // or queue things? only terminate if cancellation requested?
-        console.info('CANCEL!!!')
-        worker.terminate()
-      } else {
-        console.info('DONT CANCEL')
+      if (workerState.tag === 'Busy' && workerState.resolve === resolve) {
+        workerState.worker.terminate()
       }
     })
-    workerState = newWorkerState
-    worker.postMessage(rawElmSource)
+
+    switch (workerState.tag) {
+      case 'NotRunning':
+        workerState = { tag: 'Busy', worker: initWorker(), resolve, queue: [] }
+        workerState.worker.postMessage(rawElmSource)
+        break
+
+      case 'Idle':
+        workerState = { tag: 'Busy', worker: workerState.worker, resolve, queue: [] }
+        workerState.worker.postMessage(rawElmSource)
+        break
+
+      case 'Busy':
+        workerState.queue.push({ rawElmSource, resolve })
+        break
+    }
   })
 }
 
-const getWorker = (resolve: (result: ElmSyntax.Ast | undefined) => void): WorkerThreads.Worker => {
-  console.info('STATE', workerState.tag)
-  switch (workerState.tag) {
-    case 'NotRunning':
-      return initWorker(resolve)
-
-    case 'Idle':
-      return workerState.worker
-
-    case 'Busy':
-      workerState.worker.terminate()
-      return initWorker(resolve)
-  }
-}
-
-const initWorker = (resolve: (result: ElmSyntax.Ast | undefined) => void): WorkerThreads.Worker => {
+const initWorker = (): WorkerThreads.Worker => {
   const worker = new WorkerThreads.Worker(path.join(__dirname, './worker.js'))
 
   worker.on('error', (error) => {
     console.error(`ElmToAst`, `Worker error:`, error)
     worker.terminate()
+    if (workerState.tag === 'Busy') {
+      workerState.resolve(undefined)
+      for (const queueItem of workerState.queue) {
+        queueItem.resolve(undefined)
+      }
+    }
     workerState = { tag: 'NotRunning' }
-    resolve(undefined)
   })
 
-  worker.on('messageerror', (error) => {
-    console.error(`ElmToAst`, `Worker messageerror:`, error)
-    worker.terminate()
-    workerState = { tag: 'NotRunning' }
-    resolve(undefined)
-  })
+  const finish = (result: ElmSyntax.Ast | undefined) => {
+    if (workerState.tag === 'Busy') {
+      workerState.resolve(result)
+      const next = workerState.queue.shift()
+      if (next === undefined) {
+        workerState = { tag: 'Idle', worker: workerState.worker }
+      } else {
+        workerState.resolve = next.resolve
+        workerState.worker.postMessage(next.rawElmSource)
+      }
+    }
+  }
 
   worker.on('exit', () => {
     // Most likely, we terminated a busy worker, so there is no error to report.
-    resolve(undefined)
+    if (workerState.tag === 'Busy') {
+      workerState.resolve(undefined)
+      const next = workerState.queue.shift()
+      if (next === undefined) {
+        workerState = { tag: 'Idle', worker: initWorker() }
+      } else {
+        workerState.resolve = next.resolve
+        workerState.worker = initWorker()
+        workerState.worker.postMessage(next.rawElmSource)
+      }
+    } else {
+      workerState = { tag: 'NotRunning' }
+    }
   })
 
-  worker.on('message', (result) => {
-    workerState = { tag: 'Idle', worker }
-    resolve(result)
+  worker.on('message', (result: ElmSyntax.Ast | undefined) => {
+    if (workerState.tag === 'Busy') {
+      workerState.resolve(result)
+      const next = workerState.queue.shift()
+      if (next === undefined) {
+        workerState = { tag: 'Idle', worker: workerState.worker }
+      } else {
+        workerState.resolve = next.resolve
+        workerState.worker.postMessage(next.rawElmSource)
+      }
+    }
   })
 
   return worker
